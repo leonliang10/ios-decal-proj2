033d828f4930d8fc385618f7223a26c161a33e51
Leon Liang <jliang17@berkeley.edu>
2016-03-09 23:55:17 -0800

    implemented guess, start over, new game button; disable button pressing after winning or losing; check if a character has been entered; finished part 1

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -16,26 +16,48 @@ class GameViewController: UIViewController {
     @IBOutlet weak var userGuessCharacterInput: UITextField!
     
     let validCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    let hangmanPictures = ["hangman1.gif", "hangman2.gif", "hangman3.gif", "hangman4.gif", "hangman5.gif", "hangman6.gif", "hangman7.gif"]
 
     var hangmanWord = ""
-    var incorrectGuessedCharacters = ""
+    var incorrectGuessedCharactersForDisplay = ""
     var phraseMask = ""
     var hangmanWordArray = [Character]()
-    
-    
+    var correctGuessedCharacters = [Character]()
+    var incorrectGuessedCharacters = [Character]()
+    var numberOfIncorrectGuesses = 0
+    var finishedGame = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        newGame()
         // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func reset() {
+        finishedGame = false
+        numberOfIncorrectGuesses = 0
+        phraseMask = ""
+        correctGuessedCharacters = [Character]()
+        hangmanWord = ""
+        incorrectGuessedCharacters = [Character]()
+        userGuessCharacterInput.text = ""
+    }
+    
+    func newGame() {
         let hangmanPhrases = HangmanPhrases()
         let phrase = hangmanPhrases.getRandomPhrase()
         print(phrase)
     
+        reset()
         for i in phrase.characters.indices {
             if (phrase[i] == " ") {
-                phraseMask += " "
-//                hangmanWord += " "
+                phraseMask += "  "
+                hangmanWord += " "
             } else {
                 phraseMask += "_ "
                 hangmanWord += String(phrase[i]).lowercaseString
@@ -44,35 +66,110 @@ class GameViewController: UIViewController {
         hangmanWordArray = Array(hangmanWord.characters)
         
         correctGuessCharacterDisplay.text = phraseMask
-        hangmanImage.image = UIImage(named: "hangman1.gif")
+        hangmanImage.image = UIImage(named: hangmanPictures[numberOfIncorrectGuesses])
+        incorrectGuessesLabel.text = ""
+    }
+    
+    func startOver() {
+        reset()
+        for (var i = 0; i < hangmanWordArray.count; i++) {
+            if (hangmanWordArray[i] == " ") {
+                phraseMask += "  "
+            } else {
+                phraseMask += "_ "
+            }
+        }
+        
+        correctGuessCharacterDisplay.text = phraseMask
+        hangmanImage.image = UIImage(named: hangmanPictures[numberOfIncorrectGuesses])
         incorrectGuessesLabel.text = ""
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pressNewGameButton(sender: AnyObject) {
+        newGame()
     }
     
-    @IBAction func pressCorrectButton(sender: AnyObject) {
-        
-        
+    @IBAction func pressStartOverButton(sender: AnyObject) {
+        startOver()
     }
-
-    @IBAction func pressIncorrectButton(sender: AnyObject) {
-        let guessedCharacter = userGuessCharacterInput.text!
+    
+    
+    @IBAction func pressGuessButton(sender: AnyObject) {
+        let guessedCharacter = userGuessCharacterInput.text!.lowercaseString
+        if (finishedGame) {
+            return
+        }
+        if (guessedCharacter == "") {
+            return
+        }
         if (validCharacters.contains(guessedCharacter)) {
-            print(hangmanWordArray)
-            if (!hangmanWordArray.contains(Character(guessedCharacter))) {
-                incorrectGuessedCharacters += userGuessCharacterInput.text!
-                incorrectGuessesLabel.text = incorrectGuessedCharacters
-                incorrectGuessedCharacters += ", "
+            if (hangmanWordArray.contains(Character(guessedCharacter))) {
+                updateCorrectCharacters(guessedCharacter)
+            } else {
+                updateIncorrectCharacts(guessedCharacter)
             }
+        } else {
+            let wrongInputAlert = UIAlertController(title: "Incorrect Character", message: "Please enter ONE alpha character only", preferredStyle: .Alert)
+            let gotItButton = UIAlertAction(title: "Got it!", style: .Default, handler: nil)
+            wrongInputAlert.addAction(gotItButton)
+            showViewController(wrongInputAlert, sender: self)
         }
         
         userGuessCharacterInput.text = ""
         
     }
     
+    func updateCorrectCharacters(guessedCharacter: String) {
+        if (!correctGuessedCharacters.contains(Character(guessedCharacter))) {
+            correctGuessedCharacters.append(Character(guessedCharacter))
+            phraseMask = ""
+            var numberOfCorrectGuesses = 0
+            var numberOfSpacesInHangManWord = 0
+            for (var i = 0; i < hangmanWordArray.count; i++) {
+                if (hangmanWordArray[i] == " ") {
+                    phraseMask += "  "
+                    numberOfSpacesInHangManWord += 1
+                } else if (hangmanWordArray[i] == Character(guessedCharacter)) {
+                    phraseMask += String(hangmanWordArray[i])
+                    numberOfCorrectGuesses += 1
+                } else if (correctGuessedCharacters.contains(hangmanWordArray[i])) {
+                    phraseMask += String(hangmanWordArray[i])
+                    numberOfCorrectGuesses += 1
+                } else {
+                    phraseMask += "_ "
+                }
+            }
+            correctGuessCharacterDisplay.text = phraseMask
+            if ((numberOfCorrectGuesses + numberOfSpacesInHangManWord) == hangmanWordArray.count) {
+                let winAlert = UIAlertController(title: "You're the Champion!", message: " Great job! Press Start Over to guess the same word. Press New Game to guess a new word.", preferredStyle: UIAlertControllerStyle.Alert)
+                let hellYeahButton = UIAlertAction(title: "Hell Yeah!", style: .Default, handler: nil)
+                winAlert.addAction(hellYeahButton)
+                showViewController(winAlert, sender: self)
+                finishedGame = true
+            }
+        }
+    }
+    
+    func updateIncorrectCharacts(guessedCharacter: String) {
+        if (incorrectGuessedCharacters.contains(Character(guessedCharacter))) {
+            return
+        } else {
+            incorrectGuessedCharacters.append(Character(guessedCharacter))
+            incorrectGuessedCharactersForDisplay += userGuessCharacterInput.text!
+            incorrectGuessesLabel.text = incorrectGuessedCharactersForDisplay
+            incorrectGuessedCharactersForDisplay += ", "
+            numberOfIncorrectGuesses += 1
+            if (numberOfIncorrectGuesses < 7) {
+                hangmanImage.image = UIImage(named: hangmanPictures[numberOfIncorrectGuesses])
+            } else {
+                let loseAlert = UIAlertController(title: "You lose!", message: "Press Start Over to guess the same word. Press New Game to guess a new word.", preferredStyle: UIAlertControllerStyle.Alert)
+                let doneButton = UIAlertAction(title: "Done", style: .Default, handler: nil)
+                loseAlert.addAction(doneButton)
+                showViewController(loseAlert, sender: self)
+                finishedGame = true
+            }
+        }
+    }
     
     /*
     // MARK: - Navigation
